f66bddb5f966de2369392b2b61d7b38a3851d0f3
Endri Grigoryan <162798482+Endri94@users.noreply.github.com>
2025-06-14 15:08:10 +0300

    Delete js directory

--- js/storage.js
+++
@@ -1,8 +0,0 @@
-export function saveItems(warehouses) {
-    localStorage.setItem('warehouses', JSON.stringify(warehouses));
-}
-
-export function loadItems() {
-    const warehouses = localStorage.getItem('warehouses');
-    return warehouses ? JSON.parse(warehouses) : [];
-}
